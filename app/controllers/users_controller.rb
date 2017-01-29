36bc9aec5ce96c9bf785ae07d40d885ac4807b1a
Phil <plmok61@gmail.com>
2017-01-29 16:58:13 -0600

    sets user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
+
   def index
   end
+
   def show
     render json: {test: "Hello World!"}
   end
+
   def create
     render json: {test: "Hello World"}
   end
+
   def update
     render json: {test: "Hello World"}
   end
+
   def destroy
     render json: {test: "Hello World"}
   end
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
