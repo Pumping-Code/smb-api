3a249afa0d0ab5eb29b45ae38b929493ae255cfa
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-01-29 16:29:16 -0600

    Hello world

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def index
+  end
+  def show
+    render json: {test: "Hello World!"}
+  end
+  def create
+    render json: {test: "Hello World"}
+  end
+  def update
+    render json: {test: "Hello World"}
+  end
+  def destroy
+    render json: {test: "Hello World"}
+  end
 end
