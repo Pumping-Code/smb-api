6d47e2727121e61b51b387aed370597d7445612c
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-07-26 21:04:33 -0500

    capturing fb token

--- routes/locationsRouter.js
+++ routes/locationsRouter.js
@@ -3,6 +3,12 @@ const express = require('express');
 const router = express.Router();
 const locations = require('../controllers/locationsController');
 
+router.use((req, res, next) => {
+  const token = req.headers.token;
+  console.log('token', token);
+  next();
+});
+
 router.route('/').post(locations.sendLocation);
 
 router.route('/').get(locations.getLocations);
