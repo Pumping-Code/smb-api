eea1775528a735ab1c69a51732621c16dce23b73
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-11-28 20:40:59 -0600

    updated packages

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -8,38 +8,38 @@ exports.sendLocation = function (req, res) {
         const { lat, lng } = req.body.location;
         const { id } = req.headers;
         Location.findOneAndUpdate({ user: id }, { location: [lng, lat], user: id }, { upsert: true })
-    .then(() => {
-        Location.find({
-            location: {
-                $near: [lng, lat],
-                $maxDistance: 100,
-            },
-        })
-      .then((result) => {
-          _.remove(result, { user: id });
+            .then(() => {
+                Location.find({
+                    location: {
+                        $near: [lng, lat],
+                        $maxDistance: 100,
+                    },
+                })
+                    .then((result) => {
+                        _.remove(result, { user: id });
 
-          const arr = [];
-          _.forEach(result, ({ user }) => {
-              User.find({ id: user })
-          .then((foundUser) => {
-            // console.log('foundUser', foundUser);
-              if (foundUser[0].pushToken) {
-                  console.log('token', foundUser[0].pushToken);
-                  arr.push(foundUser[0].pushToken);
-              }
-          });
-          });
-          push(arr);
+                        const arr = [];
+                        _.forEach(result, ({ user }) => {
+                            User.find({ id: user })
+                                .then((foundUser) => {
+                                    // console.log('foundUser', foundUser);
+                                    if (foundUser[0].pushToken) {
+                                        console.log('token', foundUser[0].pushToken);
+                                        arr.push(foundUser[0].pushToken);
+                                    }
+                                });
+                        });
+                        push(arr);
 
-          res.json(result); // array of close bros
-      })
-      .catch((err) => {
-          console.log('error', err);
-      });
-    })
-    .catch((err) => {
-        console.log('error', err);
-    });
+                        res.json(result); // array of close bros
+                    })
+                    .catch((err) => {
+                        console.log('error', err);
+                    });
+            })
+            .catch((err) => {
+                console.log('error', err);
+            });
     } else {
         res.json({ error: 'location and/or fbid not sent' });
     }
@@ -47,8 +47,8 @@ exports.sendLocation = function (req, res) {
 
 exports.getLocations = function (req, res) {
     Location.find({}, 'location user when -_id').sort('-when').limit(10)
-  .then((results) => {
-      res.json(results);
-  })
-  .catch(err => res.status(400).json(err));
+        .then((results) => {
+            res.json(results);
+        })
+        .catch(err => res.status(400).json(err));
 };
