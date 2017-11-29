6fe12f11a4636935063bed1699268f4ce5382410
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-11-29 07:04:27 -0600

    setting up babel

--- config/db.js
+++ config/db.js
@@ -2,4 +2,6 @@ const connection = `mongodb://${process.env.DB_INFO}`;
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-exports.db = mongoose.connect(connection);
+exports.db = mongoose.connect(connection, {
+    useMongoClient: true,
+});
